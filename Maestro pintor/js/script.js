d234d609b8692979b1e1f504b32d63971ebdfb62
8balljar <115592008+8balljar@users.noreply.github.com>
2023-03-26 21:21:44 -0300

    pag funcional
    pagina semi-funcional, falta agregar funcionalidades y mejorar la parte estetica

---
+++ Maestro pintor/js/script.js
@@ -0,0 +1,62 @@
+const btn = document.getElementById('button');
+
+document.getElementById('form')
+ .addEventListener('submit', function(event) {
+   event.preventDefault();
+
+   btn.value = 'Sending...';
+
+   const serviceID = 'default_service';
+   const templateID = 'template_c173uwe';
+
+   emailjs.sendForm(serviceID, templateID, this)
+    .then(() => {
+      btn.value = 'Send Email';
+      alert('Sent!');
+    }, (err) => {
+      btn.value = 'Send Email';
+      alert(JSON.stringify(err));
+    });
+});
+
+window.addEventListener("scroll", function() {
+    var logo = document.getElementById("whatsapp-logo");
+    var y = window.pageYOffset;
+    if (y > 500) {
+      logo.style.bottom = "50px";
+    } else {
+      logo.style.bottom = "20px";
+    }
+  });
+
+//header//
+let slider = document.querySelector(".slider-contenedor")
+let sliderIndividual = document.querySelectorAll(".contenido-slider")
+let contador = 1;
+let width = sliderIndividual[0].clientWidth;
+let intervalo = 3000;
+
+window.addEventListener("resize", function(){
+    width = sliderIndividual[0].clientWidth;
+})
+
+setInterval(function(){
+    slides();
+},intervalo);
+
+//slider//
+
+function slides(){
+    slider.style.transform = "translate("+(-width*contador)+"px)";
+    slider.style.transition = "transform .8s";
+    contador++;
+
+    if(contador == sliderIndividual.length){
+        setTimeout(function(){
+            slider.style.transform = "translate(0px)";
+            slider.style.transition = "transform 0s";
+            contador=1;
+        },1500)
+    }
+}
+
